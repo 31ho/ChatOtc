e165d56592b0f15ea4cc19dc9338fe5d01d26d79
31ho <starting201@gmail.com>
2018-10-18 16:14:01 +0900

    1일차 Firebase를 사용한 안드로이드 채팅 앱 만들기 LoginActivity 회원가입 시 프로필 사진 Storage, Database에 저장 사진 미선택시 기본이미지를 저장

--- app/src/main/java/com/chatotc/ho/chatotc/SignupActivity.java
+++ app/src/main/java/com/chatotc/ho/chatotc/SignupActivity.java
@@ -1,28 +1,43 @@
 package com.chatotc.ho.chatotc;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.chatotc.ho.chatotc.model.UserModel;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private static final int PICK_FROM_ALBUM = 10;
     private EditText email;
     private EditText name;
     private EditText password;
     private Button signup;
+    private ImageView profile;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +50,16 @@ public class SignupActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
 
+        profile = (ImageView) findViewById(R.id.signupActivity_imageview_profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
+
         email = (EditText) findViewById(R.id.signupActivity_edittext_email);
         name = (EditText) findViewById(R.id.signupActivity_edittext_name);
         password = (EditText) findViewById(R.id.signupActivity_edittext_password);
@@ -45,25 +70,67 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(email.getText().toString() == null || name.getText().toString() == null || password.getText().toString() == null){
+                if (email.getText().toString() == null || name.getText().toString() == null || password.getText().toString() == null) {
                     return;
                 }
 
+                if(imageUri == null){
+                    imageUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.baseline_android_black_18dp);
+                    Toast.makeText(SignupActivity.this, "사진이 추가되지 않아 기본이미지를 사용합니다.", Toast.LENGTH_SHORT).show();
+                }
+
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
+                            .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                UserModel userModel = new UserModel();
-                                userModel.userName = name.getText().toString();
-                                String uid = task.getResult().getUser().getUid();
 
-                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                                    final String uid = task.getResult().getUser().getUid();
+                                    final Uri uri = Uri.fromFile(new File(imageUri.toString()));
+                                    final StorageReference riverRef = FirebaseStorage.getInstance().getReference().child("userImages").child(uid);
+                                    UploadTask uploadTask = riverRef.putFile(imageUri);
 
-                            }
-                        });
+                                    Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                                        @Override
+                                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                            if (!task.isSuccessful()) {
+                                                throw task.getException();
+                                            }
 
-            }
+                                            // Continue with the task to get the download URL
+                                            return riverRef.getDownloadUrl();
+                                        }
+                                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Uri> task) {
+                                            if (task.isSuccessful()) {
+                                                Uri downloadUri = task.getResult();
+
+                                                UserModel userModel = new UserModel();
+                                                userModel.userName = name.getText().toString();
+                                                userModel.profileImageUrl = downloadUri.toString();
+                                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+
+
+                                            } else {
+                                                // Handle failures
+                                                // ...
+
+
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == PICK_FROM_ALBUM && resultCode == RESULT_OK){
+            profile.setImageURI(data.getData());
+            imageUri = data.getData();
+        }
     }
 }
--- app/src/main/java/com/chatotc/ho/chatotc/model/UserModel.java
+++ app/src/main/java/com/chatotc/ho/chatotc/model/UserModel.java
@@ -3,5 +3,6 @@ package com.chatotc.ho.chatotc.model;
 public class UserModel {
 
     public String userName;
+    public String profileImageUrl;
 
 }
