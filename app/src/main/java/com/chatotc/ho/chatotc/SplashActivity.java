35d6b82c896a8490cc62d580d1f99a8699faa5b3
31ho <starting201@gmail.com>
2018-10-18 12:51:02 +0900

    1일차 LoginActivity layout 구성

--- app/src/main/java/com/chatotc/ho/chatotc/SplashActivity.java
+++ app/src/main/java/com/chatotc/ho/chatotc/SplashActivity.java
@@ -1,11 +1,13 @@
 package com.chatotc.ho.chatotc;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -23,6 +25,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         linearLayout = (LinearLayout) findViewById(R.id.splashactivity_linearlayout);
 
@@ -70,6 +73,9 @@ public class SplashActivity extends AppCompatActivity {
                 }
             });
             builder.create().show();
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
         }
     }
 }
