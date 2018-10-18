bfad89d1831fc4a686e7fe826527c8c91246fe11
31ho <starting201@gmail.com>
2018-10-18 14:37:48 +0900

    1일차 Firebase를 사용한 안드로이드 채팅 앱 만들기 LoginActivity Email 회원가입하여 데이터베이스에 저장

--- app/src/main/java/com/chatotc/ho/chatotc/LoginActivity.java
+++ app/src/main/java/com/chatotc/ho/chatotc/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.chatotc.ho.chatotc;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
@@ -11,7 +13,7 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 public class LoginActivity extends AppCompatActivity {
 
     private Button login;
-    private Button signin;
+    private Button signup;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
@@ -19,15 +21,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-        String splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        String splash_background = mFirebaseRemoteConfig.getString(getString(R.string.rc_color));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
 
         login = (Button) findViewById(R.id.loginActivity_button_login);
-        signin = (Button) findViewById(R.id.loginActivity_button_signin);
+        signup = (Button) findViewById(R.id.loginActivity_button_signup);
         login.setBackgroundColor(Color.parseColor(splash_background));
-        signin.setBackgroundColor(Color.parseColor(splash_background));
+        signup.setBackgroundColor(Color.parseColor(splash_background));
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/chatotc/ho/chatotc/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.chatotc.ho.chatotc.model;
+
+public class UserModel {
+
+    public String userName;
+
+}
