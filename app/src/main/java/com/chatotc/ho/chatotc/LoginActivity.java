8becdc7f9d905c69e46992d5bef304d1ac361a67
31ho <starting201@gmail.com>
2018-10-19 14:30:24 +0900

    2일차 Firebase를 사용한 안드로이드 채팅 앱 만들기 LoginActivity 로그인 후 MainActivity로의 이동

--- app/src/main/java/com/chatotc/ho/chatotc/LoginActivity.java
+++ app/src/main/java/com/chatotc/ho/chatotc/LoginActivity.java
@@ -3,33 +3,58 @@ package com.chatotc.ho.chatotc;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText id;
+    private EditText password;
+
     private Button login;
     private Button signup;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth.signOut();
         String splash_background = mFirebaseRemoteConfig.getString(getString(R.string.rc_color));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
 
+        id = (EditText) findViewById(R.id.loginActivity_edittext_id);
+        password = (EditText) findViewById(R.id.loginActivity_edittext_password);
+
         login = (Button) findViewById(R.id.loginActivity_button_login);
         signup = (Button) findViewById(R.id.loginActivity_button_signup);
         login.setBackgroundColor(Color.parseColor(splash_background));
         signup.setBackgroundColor(Color.parseColor(splash_background));
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginEvent();
+            }
+        });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +63,45 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+
+                }
+            }
+        };
+
+    }
+
+    void loginEvent(){
+        mFirebaseAuth.signInWithEmailAndPassword(id.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFirebaseAuth.removeAuthStateListener(authStateListener);
     }
 }
+
