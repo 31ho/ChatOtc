f97c762f9646d12fa8126e407a158f175ba0ae50
31ho <starting201@gmail.com>
2018-11-05 11:18:49 +0900

    9일차 Firebase를 사용한 안드로이드 채팅 앱 만들기 단체 채팅방 완성

--- app/src/main/java/com/chatotc/ho/chatotc/chat/GroupMessageActivity.java
+++ app/src/main/java/com/chatotc/ho/chatotc/chat/GroupMessageActivity.java
@@ -3,11 +3,14 @@ package com.chatotc.ho.chatotc.chat;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.chatotc.ho.chatotc.R;
+import com.chatotc.ho.chatotc.fragment.GroupMessageRecyclerViewAdapter;
 import com.chatotc.ho.chatotc.model.ChatModel;
 import com.chatotc.ho.chatotc.model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
@@ -30,6 +34,8 @@ public class GroupMessageActivity extends AppCompatActivity {
 
     EditText editText;
 
+    GroupMessageRecyclerViewAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +44,16 @@ public class GroupMessageActivity extends AppCompatActivity {
         destinationRooms = getIntent().getStringExtra("destinationRoom");
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         editText = (EditText) findViewById(R.id.groupMessageActivity_edittext);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.groupMessageActivity_recyclerview);
 
         FirebaseDatabase.getInstance().getReference()
                 .child("users")
                 .addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                users = (Map<String, UserModel>) dataSnapshot.getValue();
+                for(DataSnapshot item : dataSnapshot.getChildren()){
+                    users.put(item.getKey(), item.getValue(UserModel.class));
+                }
                 System.out.println(users.size());
                 init();
             }
@@ -54,6 +63,10 @@ public class GroupMessageActivity extends AppCompatActivity {
 
             }
         });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new GroupMessageRecyclerViewAdapter(recyclerView, destinationRooms, uid, users);
+        recyclerView.setAdapter(adapter);
     }
 
     void init(){
@@ -74,10 +87,45 @@ public class GroupMessageActivity extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
-                                editText.setText("");
+                                FirebaseDatabase.getInstance().getReference()
+                                        .child("chatrooms")
+                                        .child(destinationRooms)
+                                        .child("users")
+                                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                Map<String, Boolean> map = (Map<String, Boolean>) dataSnapshot.getValue();
+
+                                                for (String item : map.keySet()){
+                                                    if(item.equals(uid)){
+                                                        continue;
+                                                    }
+                                                    adapter.sendGcm(editText.getText().toString(), users.get(item).pushToken);
+                                                    editText.setText("");
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                            }
+                                        });
                             }
                         });
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
+                .child("chatrooms")
+                .child(destinationRooms)
+                .child("comments");
+        if(adapter.getValueEventListener() != null) {
+            databaseReference.removeEventListener(adapter.getValueEventListener());
+        }
+        finish();
     }
 }
---
+++ app/src/main/java/com/chatotc/ho/chatotc/fragment/GroupMessageRecyclerViewAdapter.java
@@ -0,0 +1,244 @@
+package com.chatotc.ho.chatotc.fragment;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.chatotc.ho.chatotc.R;
+import com.chatotc.ho.chatotc.model.ChatModel;
+import com.chatotc.ho.chatotc.model.NotificationModel;
+import com.chatotc.ho.chatotc.model.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class GroupMessageRecyclerViewAdapter extends RecyclerView.Adapter {
+
+    private List<ChatModel.Comment> comments = new ArrayList<>();
+    Map<String, UserModel> users = new HashMap<>();
+
+    private DatabaseReference databaseReference;
+    private ValueEventListener valueEventListener;
+
+    private String destinationRooms;
+    private String uid;
+    int peopleCount = 0;
+    private RecyclerView recyclerView;
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+
+    public GroupMessageRecyclerViewAdapter(RecyclerView recyclerView, String destinationRooms, String uid, Map users){
+        this.users = users;
+        this.uid = uid;
+        this.destinationRooms = destinationRooms;
+        this.recyclerView = recyclerView;
+        getMessageList();
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_message, viewGroup, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+        MessageViewHolder messageViewHolder = ((MessageViewHolder)viewHolder);
+
+        if(comments.get(i).uid.equals(uid)){
+            messageViewHolder.textView_message.setText(comments.get(i).message);
+            messageViewHolder.textView_message.setBackgroundResource(R.drawable.right_bubble);
+            messageViewHolder.linearLayout_destination.setVisibility(View.INVISIBLE);
+
+            ((LinearLayout)messageViewHolder.linearLayout_message).setGravity(Gravity.RIGHT);
+
+            setReadCounter(i, messageViewHolder.textView_readCounter_left);
+        } else {
+            Glide.with(viewHolder.itemView.getContext())
+                    .load(users.get(comments.get(i).uid).profileImageUrl)
+                    .apply(new RequestOptions().circleCrop())
+                    .into(messageViewHolder.imageView_profile);
+            messageViewHolder.textView_name.setText(users.get(comments.get(i).uid).userName);
+            messageViewHolder.linearLayout_destination.setVisibility(View.VISIBLE);
+            messageViewHolder.textView_message.setBackgroundResource(R.drawable.left_bubble);
+            messageViewHolder.textView_message.setText(comments.get(i).message);
+
+            ((LinearLayout)messageViewHolder.linearLayout_message).setGravity(Gravity.LEFT);
+
+            setReadCounter(i, messageViewHolder.textView_readCounter_right);
+        }
+
+        long unixTime = (long) comments.get(i).timestamp;
+        Date date = new Date(unixTime);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        String time = simpleDateFormat.format(date);
+        messageViewHolder.textView_timestamp.setText(time);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+
+    void getMessageList(){
+        databaseReference = FirebaseDatabase.getInstance().getReference()
+                .child("chatrooms")
+                .child(destinationRooms)
+                .child("comments");
+        valueEventListener = databaseReference
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        comments.clear();
+                        Map<String, Object> readUsersMap = new HashMap<>();
+
+                        for (DataSnapshot item : dataSnapshot.getChildren()) {
+
+                            String key = item.getKey();
+                            ChatModel.Comment comment_origin = item.getValue(ChatModel.Comment.class);
+                            ChatModel.Comment comment_motify = item.getValue(ChatModel.Comment.class);
+
+                            comment_motify.readUsers.put(uid, true);
+                            readUsersMap.put(key, comment_motify);
+
+                            comments.add(comment_origin);
+
+                        }
+                        if(comments.size() == 0){return;}
+                        if (!comments.get(comments.size() - 1).readUsers.containsKey(uid)) {
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("chatrooms")
+                                    .child(destinationRooms)
+                                    .child("comments")
+                                    .updateChildren(readUsersMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            notifyDataSetChanged();
+                                            recyclerView.scrollToPosition(getItemCount() - 1);
+                                        }
+                                    });
+
+                        } else {
+                            notifyDataSetChanged();
+                            recyclerView.scrollToPosition(getItemCount() - 1);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    public void sendGcm(String text, String pushToken){
+
+        Gson gson = new Gson();
+
+        String userName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        NotificationModel notificationModel = new NotificationModel();
+        notificationModel.to = pushToken;
+        notificationModel.notification.title = userName;
+        notificationModel.notification.text = text;
+        notificationModel.data.title = userName;
+        notificationModel.data.text = text;
+
+        RequestBody requestBody = RequestBody
+                .create(MediaType.parse("application/json; charset=utp8"), gson.toJson(notificationModel));
+
+        Request request = new Request.Builder()
+                .header("Content-Type", "application/json")
+                .addHeader("Authorization", "key=AIzaSyDw4Q607aCPSz1XkaVwqW-r-1Ql3T7Tkis")
+                .url("https://fcm.googleapis.com/fcm/send")
+                .post(requestBody)
+                .build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+            }
+        });
+    }
+
+    void setReadCounter(final int pos, final TextView textView){
+
+        if(peopleCount == 0) {
+            FirebaseDatabase.getInstance().getReference()
+                    .child("chatrooms")
+                    .child(destinationRooms)
+                    .child("users")
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Map<String, Boolean> users = (Map<String, Boolean>) dataSnapshot.getValue();
+                            peopleCount = users.size();
+                            int count = peopleCount - comments.get(pos).readUsers.size();
+                            if (count > 0) {
+                                textView.setVisibility(View.VISIBLE);
+                                textView.setText(String.valueOf(count));
+                            } else {
+                                textView.setVisibility(View.INVISIBLE);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+        } else {
+            int count = peopleCount - comments.get(pos).readUsers.size();
+            if (count > 0) {
+                textView.setVisibility(View.VISIBLE);
+                textView.setText(String.valueOf(count));
+            } else {
+                textView.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    public ValueEventListener getValueEventListener(){
+        ValueEventListener valueEventListener = this.valueEventListener;
+        return valueEventListener;
+    }
+}
